5b9622abada8e3abac9c0e0a6ead1ba0c69a3b12
Rohidas Sambhaji Kadam <rohidaskadam7777@gmail.com>
2023-02-06 16:52:34 +0530

    Switch Statement
    We have created one project of switch statement

---
+++ FirstProject/src/switchStatement/SwitchStateEx1.java
@@ -0,0 +1,23 @@
+package switchStatement;
+
+public class SwitchStateEx1 {
+
+	public static void main(String args [])
+	{
+		int r= 4;
+		
+	switch (r)
+	{
+	case 1:System.out.println(77);
+	break;
+	case 2:System.out.println(45);
+	break;
+	case 3:System.out.println(56);
+	break;
+	case 4:System.out.println(777);
+	break;
+	default : System.out.println("The number is not from provided cases");}
+	
+	}
+
+}
